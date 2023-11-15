66014b9c7bc7480dcf33c305f22c8f4971f1e009
jinyeong03 <jinyeong2808@naver.com>
2023-11-15 14:22:52 +0900

    modify Full ()

---
+++ chatApp-be/utils/io.js
@@ -0,0 +1,44 @@
+const chatController = require("../Controllers/chat.controller");
+const userController = require("../Controllers/user.controller");
+
+module.exports = function (io) {
+  //io~~~~~~
+  io.on("connection", async (socket) => {
+    console.log("client is connected", socket.id);
+
+    socket.on("login", async (userName, cb) => {
+      //유저 정보를 저장
+      try {
+        const user = await userController.saveUser(userName, socket.id);
+        const welcomeMessage = {
+          chat: `${user.name} 님이 방에 입장했습니다.`,
+          user: {
+            id: null,
+            name: "system",
+          },
+        };
+        io.emit("message", welcomeMessage);
+        cb({ ok: true, data: user });
+      } catch (error) {
+        cb({ ok: false, error: error.message });
+      }
+    });
+
+    socket.on("sendMessage", async (message, cb) => {
+      try {
+        //유저찾기 socket id로
+        const user = await userController.checkUser(socket.id);
+        //메시지 저장(유저 )
+        const newMessage = await chatController.saveChat(message, user);
+        io.emit("message", newMessage);
+        cb({ ok: true });
+      } catch (error) {
+        cb({ ok: false, error: error.message });
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log("user is disconnected");
+    });
+  });
+};
