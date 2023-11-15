9f23280ceac9c35b484b938d79fd4881a1f7eb75
jinyeong03 <jinyeong2808@naver.com>
2023-11-15 14:25:21 +0900

    modify Front

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
 import "./App.css";
+import InputField from "./components/InputField/InputField";
+import MessageContainer from "./components/MessageContainer/MessageContainer";
+import socket from "./server";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessageList((prevState) => prevState.concat(message));
+    });
+    askUserName();
+  }, []);
+
+  const askUserName = () => {
+    const userName = prompt("당신의 이름을 입력하세요.");
+    console.log("uuu", userName);
+
+    socket.emit("login", userName, (res) => {
+      if (res?.ok) {
+        setUser(res.data);
+      }
+    });
+  };
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    socket.emit("sendMessage", message, (res) => {
+      console.log("sendMessage Res", res);
+    });
+  };
+
   return (
     <div>
-      <div className="App"></div>
+      <div className="App">
+        <MessageContainer messageList={messageList} user={user} />
+
+        <InputField
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
     </div>
   );
 }
